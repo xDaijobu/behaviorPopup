1f3f187b6a15d937ac058cbb8d8b32af53d35263
msn <christwurangian@gmail.com>
2022-09-29 09:16:53 +0700

    hehehe

--- behaviorPopup/TestPopup.xaml.cs
+++ behaviorPopup/TestPopup.xaml.cs
@@ -9,3 +9,19 @@ public partial class TestPopup : Popup
 		InitializeComponent();
 	}
 }
+
+
+public class CustomBehavior : PlatformBehavior<Image>
+{
+
+#if ANDROID
+    protected override void OnAttachedTo(Image bindable, Android.Views.View platformView)
+#elif IOS
+    protected override void OnAttachedTo(Image bindable, UIKit.UIView platformView)
+#endif
+    {
+        System.Diagnostics.Debug.WriteLine("OnAttachedTo FIRE");
+
+        base.OnAttachedTo(bindable, platformView);
+    }
+}
